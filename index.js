2966bab8c69ba3c447eb2bff6018498b4e8f56db
LinaVasquez3 <lina.vasquez.mp@gmail.com>
2020-04-09 10:00:16 -0500

    handlebars implementation

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 //importar path para hacer la pagina absoluta
 const path = require('path');
 
+//importar express-handlebars
+const exphbs =require('express-handlebars');
+
 //instaciar express
 const express = require('express');
 
 //instaciar serrvidor express
 const app = express();
 
+//Registar estar de render para handlebars
+app.engine('handlebars', exphbs());
+//Use el motor de render handlebars
+app.set('view engine', 'handlebars');
+
 //hacer publica la carpeta para que se muestre el style
 app.use(express.static('public'));
 
 
-//configurar ruta inicial
+//Configurar ruta inicial - se agregó taller1
 app.get('/', function(req, res){
     //console.log('hola desde la consola');
     //res.send('hola en chrome')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-
-//abrir la shop de la pagina
-app.get('/shop', function(req, res){
+//Abrir la shop de la pagina
+app.get('/tienda', function(req, res){
     console.log('hola desde shop');
     //responder con un texto
-    res.send('Página de tienda');
+    //res.send('Página de tienda');
+    res.render('store');
 });
 
-//abrir la paglina de la pagina
+//Abrir la página de product de la página.
+app.get('/product', function(req, res){
+    console.log('hola en product');
+    //res.send('Página de product');
+    res.render('product');
+});
+
+//Abrir la página de checkout de la página.
+app.get('/checkout', function(req, res){
+    console.log('hola en checkout');
+    //res.send('Página de checkout');
+    res.render('checkout');
+});
+
+
+//abrir la pagina de lina de la pagina
 app.get('/lina', function(req, res){
     res.send('pagina de lina');
 });
